498027c205ac9e1b5caf6c3923c94cc7a6fd4484
PeekOFarad <stepan.matej99@gmail.com>
2024-04-09 21:51:07 +0200

    clean main loop, show at 0 and 12

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@ void initTime(String timezone){
 
   // Serial.println("Setting up time");
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);    // First connect to NTP server, with 0 TZ offset
-  if(!getLocalTime(&timeinfo)){
+  if(!getLocalTime(&timeinfo)) {
     // Serial.println("  Failed to obtain time");
     return;
   }
@@ -114,6 +114,172 @@ void loadPinRegs(bool zero = false){
     //load register values into bytes in PinValues arrays
     //This is needed, because the ShiftReg library uses Byte arrays to load data
     loadShiftRegs();
+}
+
+void show_date() {
+  nixie[0] = ((timeinfo.tm_year + 1900) % 100) % 10;
+  nixie[1] = ((timeinfo.tm_year + 1900) % 100) / 10;
+  nixie[2] = (timeinfo.tm_mon + 1) % 10; // Month is zero-based, so adding 1
+  nixie[3] = (timeinfo.tm_mon + 1) / 10; // Month is zero-based, so adding 1
+  nixie[4] = timeinfo.tm_mday % 10;
+  nixie[5] = timeinfo.tm_mday / 10;
+  loadPinRegs();
+}
+
+void stopwatch() {
+  //stopwatch
+  memset(nixie, 0, sizeof(nixie));
+  loadPinRegs();
+  while(digitalRead(btn) == LOW) {} //wait for btn press
+  while(digitalRead(btn) == HIGH) {} //wait for btn depress
+  unsigned long startTime = millis();
+  while(digitalRead(btn) == LOW) {  //start stopwatch
+    unsigned long currentTime = millis(); // Get the current time
+
+    // Calculate elapsed time since the start time
+    unsigned long elapsedTime = currentTime - startTime;
+
+    // Calculate hundredths of a second, seconds, and minutes
+    unsigned int hundredths = (elapsedTime / 10) % 100;
+    unsigned int seconds = (elapsedTime / 1000) % 60;
+    unsigned int minutes = (elapsedTime / 60000) % 60;
+
+    // Extract units and tens digits
+    nixie[0] = hundredths % 10;      // Units digit of hundredths
+    nixie[1] = hundredths / 10;      // Tens digit of hundredths
+    nixie[2] = seconds % 10;         // Units digit of seconds
+    nixie[3] = seconds / 10;         // Tens digit of seconds
+    nixie[4] = minutes % 10;         // Units digit of minutes
+    nixie[5] = minutes / 10;         // Tens digit of minutes
+
+    loadPinRegs(); // Assuming this function updates the display
+  }
+  while(digitalRead(btn) == HIGH) {} //wait for btn depress
+  //wait for button press before jumping out of stopwatch mode
+  while(digitalRead(btn) == LOW) {} //wait for btn press
+}
+
+void lightshow() {
+  //blink progresively faster
+  for (int i = 0; i < 20; i++) {
+    loadPinRegs(true);
+    delay(200-i*10);
+    loadPinRegs();
+    delay(200-i*10);
+  }
+  //blink fast for a bit
+  for (int i = 0; i < 10; i++) {
+    loadPinRegs(true);
+    delay(20);
+    loadPinRegs();
+    delay(20);
+  }
+  loadPinRegs(true);
+  delay(500);
+  //memset(nixie, 0, sizeof(nixie)); // zero out
+  //NUMBER WAVE
+  const int delay_wave = 30;
+  for (int l = 0; l < 9; l++) {
+    for (int i = 2; i >= 0; i--) {
+      PinValuesB = bit_set(PinValuesB, i*10+l);
+      loadShiftRegs(); 
+      delay(delay_wave);//50
+      PinValuesA = bit_set(PinValuesA, i*10+l);
+      loadShiftRegs();
+      delay(delay_wave); 
+    }
+    for (int i = 2; i >= 0; i--) {
+      PinValuesB = bit_clr(PinValuesB, i*10+l);
+      loadShiftRegs(); 
+      delay(delay_wave);
+      PinValuesA = bit_clr(PinValuesA, i*10+l);
+      loadShiftRegs();
+      delay(delay_wave); 
+    }
+  }
+  //NUMBER PONG
+  const int delay_pong = 70;
+  for (int l = 9; l >= 0; l--) {
+    //shift number l from left to right
+    for (int i = 2; i >= 0; i--) {
+      PinValuesB = bit_set(PinValuesB, i*10+l);
+      loadShiftRegs(); 
+      delay(delay_pong);
+      PinValuesB = bit_clr(PinValuesB, i*10+l);
+      PinValuesA = bit_set(PinValuesA, i*10+l);
+      loadShiftRegs();
+      delay(delay_pong);
+      PinValuesA = bit_clr(PinValuesA, i*10+l); 
+    }
+
+    //shift the number l one to the right
+    PinValuesB = bit_set(PinValuesB, l);
+    loadShiftRegs();
+    delay(delay_pong);
+    PinValuesB = bit_clr(PinValuesB, l);
+
+    //shift the number l right to left
+    for (int i = 1; i <= 2; i++) {
+      PinValuesA = bit_set(PinValuesA, i*10+l);
+      loadShiftRegs(); 
+      delay(delay_pong);
+      PinValuesA = bit_clr(PinValuesA, i*10+l);
+      PinValuesB = bit_set(PinValuesB, i*10+l);
+      loadShiftRegs();
+      delay(delay_pong);
+      PinValuesB = bit_clr(PinValuesB, i*10+l); 
+    }
+  }
+  //SHIFT IN CURRENT TIME
+  const int delay_finish = 80;
+  int display_num = 0;
+  for (int l = 0; l <= 4; l++) {
+    //first shift in hours, then minutes, then seconds
+    switch (l) {
+      case 0:
+        display_num = timeinfo.tm_hour/10;
+        break;
+      case 1:
+        display_num = timeinfo.tm_hour%10;
+        break;
+      case 2:
+        display_num = timeinfo.tm_min/10;
+        break;
+      case 3:
+        display_num = timeinfo.tm_min%10;
+        break;
+      case 4:
+        display_num = timeinfo.tm_sec/10;
+        break;
+      default:
+        break;
+    }
+    
+    for (int i = 5; i >= l; i--) {
+      /*
+      This loop index runs from 5 to l, instead of from 2 to 0 as in the previous effect.
+      This value is responsible for selecting the correct 10x range in PinValues,
+      i.e. selecting the correct pair of nixies.
+      */
+      int sel_digits = (i/2)*10;
+      if (i%2 == 1) {
+        PinValuesB = bit_set(PinValuesB, sel_digits+display_num);
+        loadShiftRegs();
+        delay(delay_finish);
+        if (i > l) { //clear the bit except for the last cycle, i.e. keep the shifted digits visible
+          PinValuesB = bit_clr(PinValuesB, sel_digits+display_num);
+        }
+      }
+      if (i%2 == 0) {
+        PinValuesA = bit_set(PinValuesA, sel_digits+display_num);
+        loadShiftRegs();
+        delay(delay_finish);
+        if (i > l) { //clear the bit except for the last cycle, i.e. keep the shifted digits visible
+          PinValuesA = bit_clr(PinValuesA, sel_digits+display_num);
+        }
+      }
+    }
+  }
 }
 
 
@@ -166,201 +332,41 @@ void loop() {
   }
 
   if (digitalRead(btn) == HIGH){
-    nixie[0] = ((timeinfo.tm_year + 1900) % 100) % 10;
-    nixie[1] = ((timeinfo.tm_year + 1900) % 100) / 10;
-    nixie[2] = (timeinfo.tm_mon + 1) % 10; // Month is zero-based, so adding 1
-    nixie[3] = (timeinfo.tm_mon + 1) / 10; // Month is zero-based, so adding 1
-    nixie[4] = timeinfo.tm_mday % 10;
-    nixie[5] = timeinfo.tm_mday / 10;
-    loadPinRegs();
-    while (digitalRead(btn) == HIGH) {} // Check if the button is released
+    show_date();
+    // Check if the button is released
+    while (digitalRead(btn) == HIGH) {} 
     prevMillis = millis();
     currentMillis = prevMillis;
-    
-    while(currentMillis - prevMillis <= 200) { //wait for a double press
-      if (digitalRead(btn) == HIGH) //stopwatch
-      {
-        while(digitalRead(btn) == HIGH) {} //wait for btn depress
-        memset(nixie, 0, sizeof(nixie));
-        loadPinRegs();
-        while(digitalRead(btn) == LOW) {} //wait for btn press
-        while(digitalRead(btn) == HIGH) {} //wait for btn depress
-        unsigned long startTime = millis();
-        while(digitalRead(btn) == LOW) {  //start stopwatch
-          unsigned long currentTime = millis(); // Get the current time
-
-          // Calculate elapsed time since the start time
-          unsigned long elapsedTime = currentTime - startTime;
-
-          // Calculate hundredths of a second, seconds, and minutes
-          unsigned int hundredths = (elapsedTime / 10) % 100;
-          unsigned int seconds = (elapsedTime / 1000) % 60;
-          unsigned int minutes = (elapsedTime / 60000) % 60;
-
-          // Extract units and tens digits
-          nixie[0] = hundredths % 10;      // Units digit of hundredths
-          nixie[1] = hundredths / 10;      // Tens digit of hundredths
-          nixie[2] = seconds % 10;         // Units digit of seconds
-          nixie[3] = seconds / 10;         // Tens digit of seconds
-          nixie[4] = minutes % 10;         // Units digit of minutes
-          nixie[5] = minutes / 10;         // Tens digit of minutes
-
-          loadPinRegs(); // Assuming this function updates the display
-        }
-        while(digitalRead(btn) == HIGH) {} //wait for btn depress
-        //wait for button press before jumping out of stopwatch mode
-        while(digitalRead(btn) == LOW) {} //wait for btn press
+    //wait for a double press
+    while(currentMillis - prevMillis <= 200) { 
+      if (digitalRead(btn) == HIGH) {
+        //wait for btn depress
+        while(digitalRead(btn) == HIGH) {} 
+        stopwatch();
       }
       currentMillis = millis();
     }
 
     delay(2000);
-    if (digitalRead(btn) == HIGH)
-    {
-      //blink progresively faster
-      for (int i = 0; i < 20; i++)
-      {
-        loadPinRegs(true);
-        delay(200-i*10);
-        loadPinRegs();
-        delay(200-i*10);
-      }
-      //blink fast for a bit
-      for (int i = 0; i < 10; i++)
-      {
-        loadPinRegs(true);
-        delay(20);
-        loadPinRegs();
-        delay(20);
-      }
-      loadPinRegs(true);
-      delay(500);
-      //memset(nixie, 0, sizeof(nixie)); // zero out
-      //NUMBER WAVE
-      const int delay_wave = 30;
-      for (int l = 0; l < 9; l++)
-      {
-        for (int i = 2; i >= 0; i--)
-        {
-          PinValuesB = bit_set(PinValuesB, i*10+l);
-          loadShiftRegs(); 
-          delay(delay_wave);//50
-          PinValuesA = bit_set(PinValuesA, i*10+l);
-          loadShiftRegs();
-          delay(delay_wave); 
-        }
-        for (int i = 2; i >= 0; i--)
-        {
-          PinValuesB = bit_clr(PinValuesB, i*10+l);
-          loadShiftRegs(); 
-          delay(delay_wave);
-          PinValuesA = bit_clr(PinValuesA, i*10+l);
-          loadShiftRegs();
-          delay(delay_wave); 
-        }
-      }
-      //NUMBER PONG
-      const int delay_pong = 70;
-      for (int l = 9; l >= 0; l--)
-      {
-        //shift number l from left to right
-        for (int i = 2; i >= 0; i--)
-        {
-          PinValuesB = bit_set(PinValuesB, i*10+l);
-          loadShiftRegs(); 
-          delay(delay_pong);
-          PinValuesB = bit_clr(PinValuesB, i*10+l);
-          PinValuesA = bit_set(PinValuesA, i*10+l);
-          loadShiftRegs();
-          delay(delay_pong);
-          PinValuesA = bit_clr(PinValuesA, i*10+l); 
-        }
-
-        //shift the number l one to the right
-        PinValuesB = bit_set(PinValuesB, l);
-        loadShiftRegs();
-        delay(delay_pong);
-        PinValuesB = bit_clr(PinValuesB, l);
-
-        //shift the number l right to left
-        for (int i = 1; i <= 2; i++)
-        {
-          PinValuesA = bit_set(PinValuesA, i*10+l);
-          loadShiftRegs(); 
-          delay(delay_pong);
-          PinValuesA = bit_clr(PinValuesA, i*10+l);
-          PinValuesB = bit_set(PinValuesB, i*10+l);
-          loadShiftRegs();
-          delay(delay_pong);
-          PinValuesB = bit_clr(PinValuesB, i*10+l); 
-        }
-
-      }
-      //SHIFT IN CURRENT TIME
-      const int delay_finish = 80;
-      int display_num = 0;
-      for (int l = 0; l <= 4; l++) {
-        //first shift in hours, then minutes, then seconds
-        switch (l)
-        {
-        case 0:
-          display_num = timeinfo.tm_hour/10;
-          break;
-        case 1:
-          display_num = timeinfo.tm_hour%10;
-          break;
-        case 2:
-          display_num = timeinfo.tm_min/10;
-          break;
-        case 3:
-          display_num = timeinfo.tm_min%10;
-          break;
-        case 4:
-          display_num = timeinfo.tm_sec/10;
-          break;
-        default:
-          break;
-        }
-        
-        for (int i = 5; i >= l; i--) {
-          /*
-          This loop index runs from 5 to l, instead of from 2 to 0 as in the previous effect.
-          This value is responsible for selecting the correct 10x range in PinValues,
-          i.e. selecting the correct pair of nixies.
-          */
-          int sel_digits = (i/2)*10;
-          if (i%2 == 1)
-          {//1,3,5
-            PinValuesB = bit_set(PinValuesB, sel_digits+display_num);
-            loadShiftRegs();
-            delay(delay_finish);
-            if (i > l) { //clear the bit except for the last cycle, i.e. keep the shifted digits visible
-              PinValuesB = bit_clr(PinValuesB, sel_digits+display_num);
-            }
-          }
-          if (i%2 == 0)
-          {//0,2,4
-            PinValuesA = bit_set(PinValuesA, sel_digits+display_num);
-            loadShiftRegs();
-            delay(delay_finish);
-            if (i > l) { //clear the bit except for the last cycle, i.e. keep the shifted digits visible
-              PinValuesA = bit_clr(PinValuesA, sel_digits+display_num);
-            }
-          }
-        }
-      }
+    if (digitalRead(btn) == HIGH) {
+      lightshow();
     }
-    // while (digitalRead(btn) != LOW) {}// Check if the button is released
 
-    
   }
   else if ((prevSec != timeinfo.tm_sec)) {
     prevSec = timeinfo.tm_sec;
     //if it's midnight, get atomic time
-    if ((timeinfo.tm_hour == 0)&&(timeinfo.tm_min == 0)&&(timeinfo.tm_sec == 0)) {
+    if ((timeinfo.tm_hour == 1)&&(timeinfo.tm_min == 0)&&(timeinfo.tm_sec == 0)) {
       wifiManager.autoConnect("AutoConnectAP");
       getLocalTime(&timeinfo);
       wifiManager.disconnect();
+    }
+    //Do a lightshow at midnight and noon
+    else if ((timeinfo.tm_hour == 0)&&(timeinfo.tm_min == 0)&&(timeinfo.tm_sec == 0)) {
+      lightshow();
+    }
+    else if ((timeinfo.tm_hour == 12)&&(timeinfo.tm_min == 0)&&(timeinfo.tm_sec == 0)) {
+      lightshow();
     }
     //Get tens and units of time
     nixie[0] = timeinfo.tm_sec%10;
